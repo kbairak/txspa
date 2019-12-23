291fbd9474962a6e5b3e2e9c1131abe1be52eb97
Konstantinos Bairaktaris <kbairak@transifex.com>
2019-12-23 21:21:40 +0200

    Fetch and show username (and JWT token)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [username, setUsername] = useState(null);
+  let [error, setError] = useState(false);
+  async function fetchUsername() {
+    try {
+      const response = await fetch('/_/jwt/');
+      const body = await response.json();
+      setUsername(body.username);
+    } catch (e) {
+      setError(true);
+    }
+  }
+  useEffect(() => { fetchUsername(); }, []);
+  if (! error) {
+    return (<h1>
+      Hello { username || 'from React' }
+    </h1>);
+  } else {
+    return (<h1 style={{color: 'red'}}>
+      Error!!!
+    </h1>);
+  }
 }
 
 export default App;
